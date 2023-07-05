01a5fe4d9ea3020dd882eae3913023ef0844b772
Alfin Cahyo Wibisono <alfincawii@gmail.com>
2023-07-05 16:54:57 +0700

    done

--- app/Http/Controllers/API/GetRepoController.php
+++ app/Http/Controllers/API/GetRepoController.php
@@ -13,10 +13,12 @@ class GetRepoController extends Controller
     public function index(Request $request, $owner, $repo)
     {
         $url = 'https://api.github.com/repos/' . $owner . '/' . $repo;
+        $agent = 'Mozilla/4.0 (compatible; MSIE 6.0; Windows NT 5.1; SV1)';
         $ch = curl_init();
         curl_setopt($ch, CURLOPT_URL, $url);
         curl_setopt($ch, CURLOPT_POST, 0);
         curl_setopt($ch, CURLOPT_RETURNTRANSFER, true);
+        curl_setopt($ch, CURLOPT_USERAGENT, $agent);
 
         $response = curl_exec($ch);
         $err = curl_error($ch);  //if you need
